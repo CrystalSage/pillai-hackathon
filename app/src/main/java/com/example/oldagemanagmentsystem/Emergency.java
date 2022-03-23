e48f621fb945948f05c930aabf2251ea7d588f9a
bourbon <amitkulkarni2028@gmail.com>
2022-03-23 09:45:20 +0530

    Add person profile and emergency section

---
+++ app/src/main/java/com/example/oldagemanagmentsystem/Emergency.java
@@ -0,0 +1,14 @@
+package com.example.oldagemanagmentsystem;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Emergency extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.emergency_section);
+    }
+}
